b0257739f18e280a43fd767b52ff988290a381da
Mauro Costa Sollar <maurosollar@gmail.com>
2020-07-14 22:26:15 -0300

    Recebe comandos

--- teleserver.py
+++ teleserver.py
@@ -1,4 +1,7 @@
 import socket
+import serial
+
+ser = serial.Serial('/dev/serial0', baudrate=9600, bytesize=8, parity="N", stopbits=1, timeout=2, xonxoff=0, rtscts=0)
 
 UDP_IP = "0.0.0.0"
 UDP_PORT = 5005
@@ -7,10 +10,10 @@ sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 sock.bind((UDP_IP, UDP_PORT))
 
-x = 0
-
 while True:
-    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    print "received message:",  data, x
-    x += 1
-    
+    print ser.readline()
+    data, addr = sock.recvfrom(1024)
+    print "received message:",  data
+    comando = data[0:1]
+    print "Comando: (" + comando + ")"
+    ser.write(comando)
