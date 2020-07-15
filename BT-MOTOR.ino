21407ed1f2f7e57e85f1193cba8c1d1e6cdc31dc
Mauro Costa Sollar <maurosollar@gmail.com>
2020-07-14 22:38:38 -0300

    Alterações código BT-MOTOR

---
+++ BT-MOTOR.ino
@@ -0,0 +1,211 @@
+#include <PS2X_lib.h>  //for MOEBIUS
+#include "FaBoPWM_PCA9685.h"
+
+//#include "servo.hpp"
+
+FaBoPWM faboPWM;
+int pos = 0;
+int MAX_VALUE = 2000;   // 电机速度限制
+int MIN_VALUE = 300;
+
+#define DIRA1 0
+#define DIRA2 1
+#define DIRB1 2
+#define DIRB2 3
+#define DIRC1 4
+#define DIRC2 5
+#define DIRD1 6
+#define DIRD2 7
+
+//电机控制，前进、后退、停止
+#define MOTORA_FORWARD(pwm)    do{faboPWM.set_channel_value(DIRA1,pwm);faboPWM.set_channel_value(DIRA2, 0);}while(0)
+#define MOTORA_STOP(x)         do{faboPWM.set_channel_value(DIRA1,0);faboPWM.set_channel_value(DIRA2, 0);}while(0)
+#define MOTORA_BACKOFF(pwm)    do{faboPWM.set_channel_value(DIRA1,0);faboPWM.set_channel_value(DIRA2, pwm);}while(0)
+
+#define MOTORB_FORWARD(pwm)    do{faboPWM.set_channel_value(DIRB1,pwm);faboPWM.set_channel_value(DIRB2, 0);}while(0)
+#define MOTORB_STOP(x)         do{faboPWM.set_channel_value(DIRB1,0);faboPWM.set_channel_value(DIRB2, 0);}while(0)
+#define MOTORB_BACKOFF(pwm)    do{faboPWM.set_channel_value(DIRB1,0);faboPWM.set_channel_value(DIRB2, pwm);}while(0)
+
+#define MOTORC_FORWARD(pwm)    do{faboPWM.set_channel_value(DIRC1,pwm);faboPWM.set_channel_value(DIRC2, 0);}while(0)
+#define MOTORC_STOP(x)         do{faboPWM.set_channel_value(DIRC1,0);faboPWM.set_channel_value(DIRC2, 0);}while(0)
+#define MOTORC_BACKOFF(pwm)    do{faboPWM.set_channel_value(DIRC1,0);faboPWM.set_channel_value(DIRC2, pwm);}while(0)
+
+#define MOTORD_FORWARD(pwm)    do{faboPWM.set_channel_value(DIRD1,pwm);faboPWM.set_channel_value(DIRD2, 0);}while(0)
+#define MOTORD_STOP(x)         do{faboPWM.set_channel_value(DIRD1,0);faboPWM.set_channel_value(DIRD2, 0);}while(0)
+#define MOTORD_BACKOFF(pwm)    do{faboPWM.set_channel_value(DIRD1,0);faboPWM.set_channel_value(DIRD2, pwm);}while(0)
+
+#define SERIAL  Serial
+
+//#define SERIAL  Serial3
+
+#define LOG_DEBUG
+
+#ifdef LOG_DEBUG
+#define M_LOG SERIAL.print
+#else
+#define M_LOG 
+#endif
+
+//PWM参数
+#define MAX_PWM   2000
+#define MIN_PWM   300
+
+int Motor_PWM = 1900;
+ 
+//控制电机运动    宏定义
+//    ↑A-----B↑   
+//     |  ↑  |
+//     |  |  |
+//    ↑C-----D↑
+void ADVANCE(uint8_t pwm_A,uint8_t pwm_B,uint8_t pwm_C,uint8_t pwm_D)
+{
+  MOTORA_BACKOFF(Motor_PWM);MOTORB_FORWARD(Motor_PWM);    
+  MOTORC_BACKOFF(Motor_PWM);MOTORD_FORWARD(Motor_PWM);    
+}
+
+//    ↓A-----B↓ 
+//     |  |  |
+//     |  ↓  |
+//    ↓C-----D↓
+void BACK()
+{
+  MOTORA_FORWARD(Motor_PWM);MOTORB_BACKOFF(Motor_PWM);
+  MOTORC_FORWARD(Motor_PWM);MOTORD_BACKOFF(Motor_PWM);
+}
+//    =A-----B↑   
+//     |   ↖ |
+//     | ↖   |
+//    ↑C-----D=
+void LEFT_1()
+{
+  MOTORA_STOP(Motor_PWM);MOTORB_FORWARD(Motor_PWM);
+  MOTORC_BACKOFF(Motor_PWM);MOTORD_STOP(Motor_PWM);
+}
+
+//    ↓A-----B↑   
+//     |  ←  |
+//     |  ←  |
+//    ↑C-----D↓
+void LEFT_2()
+{
+  MOTORA_FORWARD(Motor_PWM);MOTORB_FORWARD(Motor_PWM);
+  MOTORC_BACKOFF(Motor_PWM);MOTORD_BACKOFF(Motor_PWM);
+}
+//    ↓A-----B=   
+//     | ↙   |
+//     |   ↙ |
+//    =C-----D↓
+void LEFT_3()
+{
+  MOTORA_FORWARD(Motor_PWM);MOTORB_STOP(Motor_PWM);
+  MOTORC_STOP(Motor_PWM);MOTORD_BACKOFF(Motor_PWM);
+}
+//    ↑A-----B=   
+//     | ↗   |
+//     |   ↗ |
+//    =C-----D↑
+void RIGHT_1()
+{
+  MOTORA_BACKOFF(Motor_PWM);MOTORB_STOP(Motor_PWM);
+  MOTORC_STOP(Motor_PWM);MOTORD_FORWARD(Motor_PWM);
+}
+//    ↑A-----B↓   
+//     |  →  |
+//     |  →  |
+//    ↓C-----D↑
+void RIGHT_2()
+{
+  MOTORA_BACKOFF(Motor_PWM);MOTORB_BACKOFF(Motor_PWM);
+  MOTORC_FORWARD(Motor_PWM);MOTORD_FORWARD(Motor_PWM);
+}
+//    =A-----B↓   
+//     |   ↘ |
+//     | ↘   |
+//    ↓C-----D=
+void RIGHT_3()
+{
+  MOTORA_STOP(Motor_PWM);MOTORB_BACKOFF(Motor_PWM);
+  MOTORC_FORWARD(Motor_PWM);MOTORD_STOP(Motor_PWM);
+}
+
+//    ↑A-----B↓   
+//     | ↗ ↘ |
+//     | ↖ ↙ |
+//    ↑C-----D↓
+void rotate_1()  //tate_1(uint8_t pwm_A,uint8_t pwm_B,uint8_t pwm_C,uint8_t pwm_D) 
+{
+  MOTORA_BACKOFF(Motor_PWM);MOTORB_BACKOFF(Motor_PWM);
+  MOTORC_BACKOFF(Motor_PWM);MOTORD_BACKOFF(Motor_PWM);
+}
+
+//    ↓A-----B↑   
+//     | ↙ ↖ |
+//     | ↘ ↗ |
+//    ↓C-----D↑
+void rotate_2()  // rotate_2(uint8_t pwm_A,uint8_t pwm_B,uint8_t pwm_C,uint8_t pwm_D)
+{
+  MOTORA_FORWARD(Motor_PWM);MOTORB_FORWARD(Motor_PWM);
+  MOTORC_FORWARD(Motor_PWM);MOTORD_FORWARD(Motor_PWM);
+}
+//    =A-----B=  
+//     |  =  |
+//     |  =  |
+//    =C-----D=
+void STOP()
+{
+  MOTORA_STOP(Motor_PWM);MOTORB_STOP(Motor_PWM);
+  MOTORC_STOP(Motor_PWM);MOTORD_STOP(Motor_PWM);
+}
+
+//串口输入控制
+void UART_Control()
+{
+  char Uart_Date=0;
+  if(SERIAL.available())
+  {
+    Uart_Date = SERIAL.read();
+  }
+  switch(Uart_Date)
+  {
+     case 'A':  ADVANCE(500,500,500,500);  M_LOG("Run!\r\n"); break;
+     case 'B':  RIGHT_1();  M_LOG("Right up!\r\n");       break;
+     case 'C':  rotate_2(); M_LOG("Left rotation!\r\n");  break;      
+     case 'D':  RIGHT_3();  M_LOG("Right down!\r\n");     break;
+     case 'E':  BACK();     M_LOG("Run Back!\r\n");       break;
+     case 'F':  LEFT_3();   M_LOG("Left down!\r\n");      break;
+     case 'G':  rotate_1(); M_LOG("Right rotation!\r\n"); break;         
+     case 'H':  LEFT_1();   M_LOG("Left up!\r\n");        break;
+     case 'Z':  STOP();     M_LOG("Stop!\r\n");           break;
+     case 'z':  STOP();     M_LOG("Stop!\r\n");           break;
+     case 'd':  LEFT_2();   M_LOG("Left!\r\n");           break;
+     case 'b':  RIGHT_2();  M_LOG("Right!\r\n");          break;
+     case 'L':  Motor_PWM = 1900; STOP();  M_LOG("1900\r\n");     break;
+     case 'M':  Motor_PWM = 500;  STOP();  M_LOG("500\r\n");      break;
+   }
+}
+
+void IO_init()
+{
+  STOP();
+}
+
+void setup()
+{
+  SERIAL.begin(9600);
+  IO_init();
+  if(faboPWM.begin()) 
+  {
+    Serial.println("Find PCA9685");
+    faboPWM.init(300);
+  }
+  faboPWM.set_hz(50);
+  SERIAL.print("Start");
+}
+
+
+
+void loop()
+{
+    UART_Control();//串口接收处理 
+    //CAR_Control();//小车控制
+}
